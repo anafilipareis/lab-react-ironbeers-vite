8097d61cd9f7671f4dc8bc0576277b8e9fad4191
Ana Filipa <anafilipa737@gmail.com>
2024-02-21 14:37:09 +0000

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
+import Navbar from "./components/Navbar";      // <== IMPORT
+import HomePage from "./pages/HomePage";       // <== IMPORT
+import AllBeersPage from "./pages/AllBeersPage";     // <== IMPORT
+import AddBeerPage from "./pages/AddBeerPage";   // <== IMPORT
+import BeerDetailsPage from "./pages/BeerDetailsPage";         // <== IMPORT
+import RandomBeerPage from "./pages/RandomBeerPage";
+ 
+import { Routes, Route } from "react-router-dom";  // <== IMPORT
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React IronBeers</h1>
+      <Navbar />
+      
+      {/*   Add <Route /> components between <Routes> and </Routes>   */} 
+      <Routes>
+        <Route path="/" element={<HomePage />} /> 
+        <Route path="/beers" element={<AllBeersPage />} />
+        <Route path="/random-beer" element={<RandomBeerPage />} />
+        <Route path="/new-beer" element={<AddBeerPage />} />
+        <Route path="/beers/:beerId" element={<BeerDetailsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
+// ReactDOM.createRoot creates a root in the DOM for the React application
+// documeny.getElementById("root") retrieves the HTML element with the id "root" to serve as the root container for the app
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
